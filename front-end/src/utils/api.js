a2170a877976d64a4b4abff51ea397b3c3842608
Ethan Wiegert <ewiegert99@gmail.com>
2023-11-27 11:36:22 -0500

    Correcting API for correct parameter.

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -58,15 +58,15 @@ async function fetchJson(url, options, onCancel) {
  *  a promise that resolves to a possibly empty array of reservation saved in the database.
  */
 
-export async function listReservations({date:dateToday}, signal) {
+export async function listReservations(params, signal) {
   const url = new URL(`${API_BASE_URL}/reservations`);
-  Object.entries(dateToday).forEach(([key, value]) =>
-    url.searchParams.append(key, value.toString())
+  Object.entries(params).forEach(([key, value]) =>
+  url.searchParams.append(key, value.toString())
   );
   return await fetchJson(url, { headers, signal }, [])
-    .then(formatReservationDate)
-    .then(formatReservationTime);
-}
+  .then(formatReservationDate)
+  .then(formatReservationTime);
+  }
 
 export async function createReservation(reservation, signal){
   const url = `${API_BASE_URL}/reservation`;
