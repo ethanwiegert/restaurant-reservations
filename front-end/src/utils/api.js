8c9878c636bdee23290a28e1648a5132385cf105
Ethan Wiegert <ewiegert99@gmail.com>
2023-12-07 16:12:45 -0500

    Correcting API link.

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -114,7 +114,7 @@ export async function updateTable(data, signal) {
 }
 
 export async function deleteTable(data, signal){
-  const url = `${API_BASE_URL}/tables/${data.data.tableId}/seat`;
+  const url = `${API_BASE_URL}/tables/${data.data.table_id}/seat`;
   const options = { method: "DELETE", signal };
   return await fetchJson(url, options);
 }
