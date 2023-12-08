91f7ffca8b14d87b314f80603fd3a090ce22afb8
Ethan Wiegert <ewiegert99@gmail.com>
2023-12-08 18:21:52 -0500

    Changing variable names.

--- front-end/src/reservations/EditReservation.js
+++ front-end/src/reservations/EditReservation.js
@@ -16,8 +16,8 @@ function EditReservation(){
         reservation_time: "",
         people: 0,
       };
-
-    const [reservation, setReservation] = useState({ ...initialFormData });
+    const [reservation, setReservation] = useState(null);
+    const [updatedReservation, setUpdatedReservation] = useState({ ...initialFormData });
     const [formError, setFormError] = useState(null);
 
     useEffect(readReservation, [reservationId])
@@ -39,15 +39,15 @@ function EditReservation(){
 
     
     const handleNumber = ({ target }) => {
-        setReservation({
-          ...reservation,
+        setUpdatedReservation({
+          ...updatedReservation,
           [target.name]: Number(target.value),
         });
       };
 
     function handleChange({target}){
-        setReservation({
-            ...reservation,
+        setUpdatedReservation({
+            ...updatedReservation,
             [target.name]: target.value
         })
     }
@@ -63,8 +63,8 @@ function EditReservation(){
         const abortController = new AbortController();
         setFormError(null);
         try{
-          let response=await updateReservation(reservation, abortController.signal)  
-          history.push(`/dashboard?date=${reservation.reservation_date}`)
+          let response=await updateReservation(updatedReservation, abortController.signal)  
+          history.push(`/dashboard?date=${updatedReservation.reservation_date}`)
         } catch (e){
           setFormError(e)
         }
@@ -89,32 +89,32 @@ function EditReservation(){
             <form className="row" onSubmit={handleSubmit}>
                 <div className="col-md-6">
                 <label className="form-label">First Name</label>
-                <input id="first_name" name="first_name" type="text" value={reservation.first_name} onChange={handleChange} required/>
+                <input id="first_name" name="first_name" type="text" value={updatedReservation.first_name} onChange={handleChange} required/>
                 </div>
 
                 <div className="col-md-6">
                 <label className="form-label">Last Name</label>
-                <input id="last_name" name="last_name" type="text" value={reservation.last_name} onChange={handleChange}  required/>
+                <input id="last_name" name="last_name" type="text" value={updatedReservation.last_name} onChange={handleChange}  required/>
                 </div>
 
                 <div className="col-md-6">
                 <label className="form-label">Mobile Number</label>
-                <input id="mobile_number" name="mobile_number" type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" value={reservation.mobile_number} onChange={handleChange}  required/>
+                <input id="mobile_number" name="mobile_number" type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" value={updatedReservation.mobile_number} onChange={handleChange}  required/>
                 </div>
 
                 <div className="col-md-6">
                 <label className="form-label">Reservation Date</label>
-                <input id="reservation_date" name="reservation_date" type="date" value={reservation.reservation_date} onChange={handleChange} required/>
+                <input id="reservation_date" name="reservation_date" type="date" value={updatedReservation.reservation_date} onChange={handleChange} required/>
                 </div>
                 
                 <div className="col-md-6">
                 <label className="form-label">Reservation Time</label>
-                <input id="reservation_time" name="reservation_time" type="time" value={reservation.reservation_time} onChange={handleChange} required/>
+                <input id="reservation_time" name="reservation_time" type="time" value={updatedReservation.reservation_time} onChange={handleChange} required/>
                 </div>
 
                 <div className="col-md-6">
                 <label className="form-label">People</label>
-                <input id="people" name="people" type="number" value={reservation.people} onChange={handleNumber}  required/>
+                <input id="people" name="people" type="number" value={updatedReservation.people} onChange={handleNumber}  required/>
                 </div>
 
                 <button type="cancel" className="btn btn-secondary mb-2" onClick={handleCancel}>Cancel</button>
